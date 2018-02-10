9eb1bbb4b8b6fb7ee32dd4d65b5a35ef0f54371e
Stupyspyy <sagarpratik1@gmail.com>
2018-02-10 01:42:26 -0500

    updated

--- Data Structure/single_linklist.c
+++ Data Structure/single_linklist.c
@@ -1,26 +1,77 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct nodeP{
+struct node
+{
 int info;
 struct node *next;
-}start=NULL;
-struct node* create()
+}*start=NULL;
+struct node *create()
 {
-  struct node* newnode;
+  struct node *newnode;
   newnode=(struct node*)malloc(sizeof(struct node));
   return (newnode);
 }
 void insert()
 {
-    struct node *temp,*n;
+    struct node *temp,*p;
     temp=create();
     temp->next=NULL;
-    if(temp==NULL)
-        scanf("%d",&temp->info);
+    scanf("%d",&temp->info);
+    if(start==NULL)
+    {
+        start=temp;
+    }
     else
     {
-        n=start;
-        while(n->!=NULL)
-        n=n->next;
+        p=start;
+        while(p->next!=NULL)
+        p=p->next;
+        p->next=temp;
+    }
+}
+void display()
+{
+    struct node *p;
+    p=start;
+    while(p!=NULL)
+    {
+        printf("%d\n",p->info);
+        p=p->next;
+    }
+}
+void delete(int temp)
+{
+    stack nodee *p,temp;
+    p=start;
+    if(temp==start->info)
+    {
+        start=start->next;
+        free(p);
+    }
+    else
+    {
+        while(p->info!=temp)
+        {
+            temp=p;
+            p=p->next;
+        }
+        temp=
+    }
+}
+int main()
+{
+    int choice;
+    while(1)
+    {
+        printf("1.insert\n2.display\n3.exit");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+        case 1: insert();
+        break;
+        case 2: display();
+        break;
+        case 3: exit(0);
+        }
     }
 }
